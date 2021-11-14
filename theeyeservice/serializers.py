0442a7bcdc406b51a92aef02642cf5d580dd3c71
Arthur Moreira <arthurmoreirabsi@gmail.com>
2021-11-14 14:21:54 -0300

    Removing empty or null values from the response

--- theeyeservice/serializers.py
+++ theeyeservice/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from theeyeservice.models import Event, Payload, Form
+from collections import OrderedDict
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,12 +10,22 @@ class FormSerializer(serializers.ModelSerializer):
         model = Form
         fields = ['first_name', 'last_name']
 
+    def to_representation(self, instance):
+        logger.info('Removing empty values from Form representation')
+        result = super(FormSerializer, self).to_representation(instance)
+        return OrderedDict([(key, result[key]) for key in result if result[key]])
+
 class PayloadSerializer(serializers.ModelSerializer):
     form = FormSerializer(many=False, required=False, allow_null=True)
 
     class Meta:
         model = Payload
         fields = ['host', 'path', 'element', 'form']
+
+    def to_representation(self, instance):
+        logger.info('Removing empty values from Payload representation')
+        result = super(PayloadSerializer, self).to_representation(instance)
+        return OrderedDict([(key, result[key]) for key in result if result[key]])
 
 class EventSerializer(serializers.ModelSerializer):
     data = PayloadSerializer(many=False)
@@ -24,10 +35,6 @@ class EventSerializer(serializers.ModelSerializer):
         fields = ['session_id', 'category', 'name', 'timestamp', 'data']
 
     def create(self, validated_data):
-        """
-        Overriding the create method for the event serializer to save the payload and check if there is a Form in the payload.
-        Other scenarios are possible, but that is to be defined.
-        """
         logger.info('Saving events - start')
 
         form = Form.objects.create(**validated_data.get('data').pop('form')) if 'form' in validated_data.get('data') else None
